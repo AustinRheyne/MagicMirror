b2080fae534c6e2a37e4bfb90ccb0a90af0967c6
Austin Rheyne <57242191+AustinRheyne@users.noreply.github.com>
2022-02-27 13:48:30 -1000

    Add files via upload

--- currentTime.py
+++ currentTime.py
@@ -5,12 +5,11 @@ class CurrentTime:
     def __init__(self, screen):
         h = screen.get_height()
         self.font = pygame.font.SysFont('calibri', int((200*h/1080)))
-        self.text = self.font.render(datetime.now().strftime("%H:%M:%S"), False, (255, 255, 255))
+        self.text = self.font.render(datetime.now().strftime("%H:%M:%S"), True, (255, 255, 255))
         self.rect = self.text.get_rect()
         self.screen = screen
-
     def update(self):
-        self.text = self.font.render(datetime.now().strftime("%H:%M:%S"), False, (255, 255, 255))
+        self.text = self.font.render(datetime.now().strftime("%H:%M:%S"), True, (255, 255, 255))
         self.text = pygame.transform.rotate(self.text, 90)
         self.rect = self.text.get_rect()
         self.rect.centery = self.screen.get_rect().centery
--- message.py
+++ message.py
@@ -1,21 +1,27 @@
 import pygame
 import json
-import datetime
+from datetime import datetime
+import random
 class Message:
     def __init__(self, screen):
-        h = screen.get_height()
-        self.currentMessage = "Don't forget to drink some water!"
-        self.font = pygame.font.SysFont('calibri', int((2000*h/1080)/len(self.currentMessage)))
-        self.text = self.font.render(self.currentMessage, False, (255, 255, 255))
-        self.rect = self.text.get_rect()
-        self.screen = screen
+        with open('goodDay.json', "r") as f:
+            data = json.load(f)
 
-    def update(self):
-        self.text = self.font.render(self.currentMessage, False, (255, 255, 255))
+        if int(datetime.now().strftime("%H")) > 12:
+            self.currentMessage = random.choice(data["Night"])
+        else:
+            self.currentMessage = random.choice(data["Morning"])
+
+        h = screen.get_height()
+        self.font = pygame.font.SysFont('calibri', int((2000*h/1080)/len(self.currentMessage)))
+        self.text = self.font.render(self.currentMessage, True, (255, 255, 255))
         self.text = pygame.transform.rotate(self.text, 90)
         self.rect = self.text.get_rect()
         self.rect.centery = self.screen.get_rect().centery
         self.rect.right = self.screen.get_rect().right - 20
+        self.screen = screen
+
+    def update(self):
         self.blit_me()
 
     def blit_me(self):
